23d048729bea1fc0ab7771d9ed3f265c7f864742
bryanlowcc <bryanlowcc@gmail.com>
2021-07-06 03:54:31 +0800

    final fix for deployment

--- actions/actions.py
+++ actions/actions.py
@@ -37,23 +37,22 @@ class ActionSessionStart(Action):
     def name(self) -> Text:
         return "action_session_start"
 
-    async def run(
-      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
-        # time = int(datetime.now().strftime("%H"))
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        time = int(datetime.now().strftime("%H"))
 
-        # if time >= 5 and time < 12:
-        #     response = "Good Morning! How may I assist you today?\ntype help for the list of features I'm currently capable of"
-        #     dispatcher.utter_message(text=response)
+        if time >= 5 and time < 12:
+            response = "Good Morning! How may I assist you today?"
+            dispatcher.utter_message(text=response)
 
-        # elif time >= 12 and time < 17:
-        #     response = "Good Afternoon! How may I assist you today?\ntype help for the list of features I'm currently capable of"
-        #     dispatcher.utter_message(text=response)
+        elif time >= 12 and time < 17:
+            response = "Good Afternoon! How may I assist you today?"
+            dispatcher.utter_message(text=response)
 
-        # else:
-        #     response = "Good Evening! How may I assist you today?\ntype help for the list of features I'm currently capable of"
-        #     dispatcher.utter_message(text=response)
-        return [SessionStarted(), ActionExecuted("action_welcome"), ActionExecuted("action_listen")]
+        else:
+            response = "Good Evening! How may I assist you today?"
+            dispatcher.utter_message(text=response)
+        dispatcher.utter_message(text="type help for the list of features I'm currently capable of.")
+        return [SessionStarted(), ActionExecuted("action_listen")]
 
 class WelcomeMessage(Action):
     def name(self) -> Text:
@@ -133,19 +132,20 @@ class Covid(Action):
         total_vax1 = vax_data[4]
         total_vax2 = vax_data[5]
 
-        # response =  f"Here are the current covid statistics for Malaysia:\nNew cases today = {cases_today}\nNew cases yesterday = {cases_yest}\nCurrent total active cases = {total_cases}\nCurrent total deaths = {total_deaths}\nTotal population in Malaysia = {total_pop}\nTotal tests = {total_tests}\nTest ratio per 100 people: {test_ratio}\nNew 1st dose vaccinations today = {vax1_today}\nNew 2nd dose vaccinations today = {vax2_today}\nTotal 1st dose vaccinations = {total_vax1}\nTotal 2nd dose vaccinations = {total_vax2}"
+        response =  f"Here are the current covid statistics for Malaysia:\nNew cases today = {cases_today}\nNew cases yesterday = {cases_yest}\nCurrent total active cases = {total_cases}\nCurrent total deaths = {total_deaths}\nTotal population in Malaysia = {total_pop}\nTotal tests = {total_tests}\nTest ratio per 100 people: {test_ratio}\nNew 1st dose vaccinations today = {vax1_today}\nNew 2nd dose vaccinations today = {vax2_today}\nTotal 1st dose vaccinations = {total_vax1}\nTotal 2nd dose vaccinations = {total_vax2}"
 
-        dispatcher.utter_message(text="Here are the current covid statistics for Malaysia:")
-        dispatcher.utter_message(text=f"New cases today = {cases_today}")
-        dispatcher.utter_message(text=f"New cases yesterday = {cases_yest}")
-        dispatcher.utter_message(text=f"Current total active cases = {total_cases}")
-        dispatcher.utter_message(text=f"Current total deaths = {total_deaths}")
-        dispatcher.utter_message(text=f"Total population in Malaysia = {total_pop}")
-        dispatcher.utter_message(text=f"Total tests = {total_tests}")
-        dispatcher.utter_message(text=f"Test ratio per 100 people: {test_ratio}")
-        dispatcher.utter_message(text=f"New 1st dose vaccinations today = {vax1_today}")
-        dispatcher.utter_message(text=f"New 2nd dose vaccinations today = {vax2_today}")
-        dispatcher.utter_message(text=f"Total 1st dose vaccinations = {total_vax1}")
-        dispatcher.utter_message(text=f"Total 2nd dose vaccinations = {total_vax2}")
+        dispatcher.utter_message(text=response)
+        # dispatcher.utter_message(text="Here are the current covid statistics for Malaysia:")
+        # dispatcher.utter_message(text=f"New cases today = {cases_today}")
+        # dispatcher.utter_message(text=f"New cases yesterday = {cases_yest}")
+        # dispatcher.utter_message(text=f"Current total active cases = {total_cases}")
+        # dispatcher.utter_message(text=f"Current total deaths = {total_deaths}")
+        # dispatcher.utter_message(text=f"Total population in Malaysia = {total_pop}")
+        # dispatcher.utter_message(text=f"Total tests = {total_tests}")
+        # dispatcher.utter_message(text=f"Test ratio per 100 people: {test_ratio}")
+        # dispatcher.utter_message(text=f"New 1st dose vaccinations today = {vax1_today}")
+        # dispatcher.utter_message(text=f"New 2nd dose vaccinations today = {vax2_today}")
+        # dispatcher.utter_message(text=f"Total 1st dose vaccinations = {total_vax1}")
+        # dispatcher.utter_message(text=f"Total 2nd dose vaccinations = {total_vax2}")
 
         return [UserUtteranceReverted()]        
